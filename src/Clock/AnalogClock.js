b9d614114b236aff63ef31737b5e788bb2345c2d
edwingon <edwin@avenueimedia.com>
2019-12-01 12:55:53 -0800

    Designing analog clock

--- src/Clock/AnalogClock.js
+++ src/Clock/AnalogClock.js
@@ -1,6 +1,3 @@
-//Angles of an analog clock depend on the following
-// angleHr = 0.5deg x (60 x hr + min)
-// angleMin = 6deg x M
 import React, { Component } from 'react';
 
 class AnalogClock extends Component {
@@ -15,16 +12,37 @@ class AnalogClock extends Component {
 		}
 	}
 
-	getHourAngle(){
-		// We get a number from (12am) 0 to 23 (11pm) 
-		// Need to account for difference
-		// 0 - 12 <- if 0 then 12 
-		// 1 - 12 
-		// 13-1, 14-2, 15-3, 16-4, 17-5, 18-6, 19-7, 20-8,
-		// 21-9, 22-10, 23-11
+	// We get a number from (12am) 0 to 23 (11pm) 
+	// 0 - 12 <- if 0 then 12 
+	// 1 - 12 is an exact mathing 
+	// 13-1, 14-2, 15-3, 16-4, 17-5, 18-6, 19-7, 20-8,
+	// 21-9, 22-10, 23-11 
 
+	getRegularTime(hour){
+		if (hour == 0){
+			return 12
+		}else if(hour > 12){
+			return hour - 12
+		} else {
+			return hour
+		}
+	}
 
+	//Angles of an analog clock depend on the following
 
-		const  
+	// angleHr = 0.5 deg x (60 x hr + min)
+	getHourAngle(hour,min){
+		//Each hour has space of 30deg difference
+		const angle = 0.5*(60* hour + min);
+
+		return angle
+	}
+
+	// angleMin = 6 deg x M
+	getMinuteAngle(min){
+		//Each minute has 6deg difference
+		const angle = 6 * min;
+
+		return angle
 	}
 }
